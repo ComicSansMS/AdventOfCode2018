3e501a04ec2b4e6a814b3b865712da4ded4ec1bd
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-12-12 06:21:16 +0100

    first optimization: lookup

--- 11/fuel_cell_grid.cpp
+++ 11/fuel_cell_grid.cpp
@@ -18,7 +18,20 @@ int calculatePowerLevel(int x, int y, int grid_serial_number)
     return power_level;
 }
 
-std::tuple<int, int, int> getLargestSquare(int grid_serial_number, int size)
+namespace {
+std::vector<int> precomputeGrid(int grid_serial_number)
+{
+    std::vector<int> v;
+    v.reserve(300 * 300);
+    for(int y = 0; y < 300; ++y) {
+        for(int x = 0; x < 300; ++x) {
+            v.push_back(calculatePowerLevel(x + 1, y + 1, grid_serial_number));
+        }
+    }
+    return v;
+}
+
+std::tuple<int, int, int> getLargestSquare_impl(int size, std::vector<int> const& grid)
 {
     int max_power = 0;
     int max_x = -1;
@@ -30,7 +43,7 @@ std::tuple<int, int, int> getLargestSquare(int grid_serial_number, int size)
                 for(int inner_y : ranges::iota_view(y, y + size)) {
                     assert((inner_x >= 1) && (inner_x <= 300));
                     assert((inner_y >= 1) && (inner_y <= 300));
-                    power += calculatePowerLevel(inner_x, inner_y, grid_serial_number);
+                    power += grid[(inner_y - 1)* 300 + (inner_x - 1)];
                 }
             }
             if(power > max_power) {
@@ -43,14 +56,23 @@ std::tuple<int, int, int> getLargestSquare(int grid_serial_number, int size)
     return std::make_tuple(max_power, max_x, max_y);
 }
 
+}
+
+std::tuple<int, int, int> getLargestSquare(int grid_serial_number, int size)
+{
+    std::vector<int> const grid = precomputeGrid(grid_serial_number);
+    return getLargestSquare_impl(size, grid);
+}
+
 std::tuple<int, int, int, int> getLargestOfAllSquares(int grid_serial_number)
 {
+    std::vector<int> const grid = precomputeGrid(grid_serial_number);
     int max_power = 0;
     int max_x = -1;
     int max_y = -1;
     int max_size = -1;
     for(int sq_size : ranges::iota_view(1, 301)) {
-        auto const [power, x, y] = getLargestSquare(grid_serial_number, sq_size);
+        auto const [power, x, y] = getLargestSquare_impl(sq_size, grid);
         if(power > max_power) {
             max_power = power;
             max_x = x;
