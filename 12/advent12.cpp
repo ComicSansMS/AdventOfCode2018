65cfa4d4391c0d4217926086dd60ae5e72b3b33e
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-12-13 16:00:17 +0100

    day 12 part 2

--- 12/advent12.cpp
+++ 12/advent12.cpp
@@ -39,12 +39,18 @@ int main(int argc, char* argv[])
     auto plants = Plants::parseFromInput(*input);
     for(int i=0; i<20; ++i) { plants.simulateStep(); }
     std::cout << "First result is " << plants.accumulate() << std::endl;
-    //plants = Plants::parseFromInput(*input);
-    //for(int64_t i=0; i<50000000000; ++i) {
-    //    if((i%1000000) == 0) { std::cout << '.'; }
-    //    plants.simulateStep();
-    //}
-    //std::cout << "Second result is " << plants.accumulate() << std::endl;
+    plants = Plants::parseFromInput(*input);
+    int64_t last_step = 0;
+    int64_t diff = 0;
+    int64_t target_step = 50000000000;
+    int64_t precompute_steps = 500;
+    for(int64_t i=0; i<precompute_steps; ++i) {
+        plants.simulateStep();
+        int64_t const new_step = plants.accumulate();
+        diff = new_step - last_step;
+        last_step = new_step;
+    }
+    std::cout << "Second result is " << (plants.accumulate() + diff * (target_step - precompute_steps)) << std::endl;
 
     return 0;
 }
