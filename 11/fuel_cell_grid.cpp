fea28d140c4c6da6e746e35aad107ad292b46f92
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-12-13 12:20:10 +0100

    even faster

--- 11/fuel_cell_grid.cpp
+++ 11/fuel_cell_grid.cpp
@@ -32,6 +32,54 @@ std::vector<int> precomputeGrid(int grid_serial_number)
     return v;
 }
 
+std::tuple<int, int, int> getLargestSquare_impl_row_lookup(int size, std::vector<int> const& grid, std::vector<int>& rows)
+{
+    int max_power = std::numeric_limits<int>::min();
+    int max_x = -1;
+    int max_y = -1;
+    int current_row_index = 0;
+    int const limit_outer_loop = (300 - size + 1);
+    for(int x = 1; x < limit_outer_loop; ++x) {
+        int power = 0;
+        for(int y = 1; y < limit_outer_loop; ++y) {
+            if(y == 1) {
+                current_row_index = 0;
+                int const limit_inner_x = x+size;
+                for(int inner_x = x; inner_x < limit_inner_x; ++inner_x) {
+                    rows[current_row_index] = 0;
+                    int const limit_inner_y = y+size;
+                    for(int inner_y = y; inner_y < limit_inner_y; ++inner_y) {
+                        assert((inner_x >= 1) && (inner_x <= 300));
+                        assert((inner_y >= 1) && (inner_y <= 300));
+                        rows[current_row_index] += grid[(inner_y - 1)* 300 + (inner_x - 1)];
+                    }
+                    power += rows[current_row_index];
+                    ++current_row_index;
+                }
+                current_row_index = 0;
+            } else {
+                // remove upper row
+                power -= rows[current_row_index];
+                rows[current_row_index] = 0;
+                int const limit_xx = x + size;
+                for(int xx = x; xx < limit_xx; ++xx) {
+                    // add lower row
+                    rows[current_row_index] += grid[((y + size) - 2) * 300 + (xx - 1)];
+                }
+                power += rows[current_row_index];
+                ++current_row_index;
+                if(current_row_index == size) { current_row_index = 0; }
+            }
+            if(power > max_power) {
+                max_power = power;
+                max_x = x;
+                max_y = y;
+            }
+        }
+    }
+    return std::make_tuple(max_power, max_x, max_y);
+}
+
 std::tuple<int, int, int> getLargestSquare_impl(int size, std::vector<int> const& grid)
 {
     int max_power = std::numeric_limits<int>::min();
@@ -41,8 +89,8 @@ std::tuple<int, int, int> getLargestSquare_impl(int size, std::vector<int> const
         int power = 0;
         for(int y : ranges::iota_view(1, (300 - size) + 1)) {
             if(y == 1) {
-                for(int inner_x : ranges::iota_view(x, x + size)) {
-                    for(int inner_y : ranges::iota_view(y, y + size)) {
+                for(int inner_y : ranges::iota_view(y, y + size)) {
+                    for(int inner_x : ranges::iota_view(x, x + size)) {
                         assert((inner_x >= 1) && (inner_x <= 300));
                         assert((inner_y >= 1) && (inner_y <= 300));
                         power += grid[(inner_y - 1)* 300 + (inner_x - 1)];
@@ -65,12 +113,64 @@ std::tuple<int, int, int> getLargestSquare_impl(int size, std::vector<int> const
     }
     return std::make_tuple(max_power, max_x, max_y);
 }
+
+std::tuple<int, int, int> getLargestSquare_impl_row_sums(int size, std::vector<int> const& grid, std::vector<int> const& row_sums)
+{
+    int max_power = std::numeric_limits<int>::min();
+    int max_x = -1;
+    int max_y = -1;
+    for(int x : ranges::iota_view(1, (300 - size) + 1)) {
+        int power = 0;
+        for(int y : ranges::iota_view(1, (300 - size) + 1)) {
+            if(y == 1) {
+                int const limit_inner_y = y+size;
+                for(int inner_y = y; inner_y < limit_inner_y; ++inner_y) {
+                    if(x == 1) {
+                        power += row_sums[(inner_y - 1)* 300 + (x + size - 2)];
+                    } else {
+                        power += row_sums[(inner_y - 1)* 300 + (x + size - 2)] -
+                                 row_sums[(inner_y - 1)* 300 + (x - 2)];
+                    }
+                }
+            } else {
+                int diff_minus = 0;
+                int diff_plus = 0;
+                if(x == 1) {
+                    diff_minus = row_sums[(y - 2)* 300 + (x + size - 2)];
+                    diff_plus = row_sums[((y + size) - 2)* 300 + (x + size - 2)];
+                } else {
+                    diff_minus = row_sums[(y - 2)* 300 + (x + size - 2)] -
+                                  row_sums[(y - 2)* 300 + (x - 2)];
+                    diff_plus = row_sums[((y + size) - 2)* 300 + (x + size - 2)] -
+                                 row_sums[((y + size) - 2)* 300 + (x - 2)];
+                }
+                // remove upper row
+                power -= diff_minus;
+                // add lower row
+                power += diff_plus;
+            }
+            if(power > max_power) {
+                max_power = power;
+                max_x = x;
+                max_y = y;
+            }
+        }
+    }
+    return std::make_tuple(max_power, max_x, max_y);
+}
+
+
 }
 
 std::tuple<int, int, int> getLargestSquare(int grid_serial_number, int size)
 {
     std::vector<int> const grid = precomputeGrid(grid_serial_number);
-    return getLargestSquare_impl(size, grid);
+    std::vector<int> row_sums;
+    row_sums.reserve(300 * 300);
+    for(int y : ranges::view::iota(0, 300)) {
+        std::partial_sum(begin(grid) + y*300, begin(grid) + (y+1)*300, std::back_inserter(row_sums));
+    }
+    return getLargestSquare_impl_row_sums(size, grid, row_sums);
 }
 
 std::tuple<int, int, int, int> getLargestOfAllSquares(int grid_serial_number)
@@ -80,8 +180,13 @@ std::tuple<int, int, int, int> getLargestOfAllSquares(int grid_serial_number)
     int max_x = -1;
     int max_y = -1;
     int max_size = -1;
+    std::vector<int> row_sums;
+    row_sums.reserve(300 * 300);
+    for(int y : ranges::view::iota(0, 300)) {
+        std::partial_sum(begin(grid) + y*300, begin(grid) + (y+1)*300, std::back_inserter(row_sums));
+    }
     for(int sq_size : ranges::iota_view(1, 301)) {
-        auto const [power, x, y] = getLargestSquare_impl(sq_size, grid);
+        auto const [power, x, y] = getLargestSquare_impl_row_sums(sq_size, grid, row_sums);
         if(power > max_power) {
             max_power = power;
             max_x = x;
