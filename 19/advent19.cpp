0fdec17736ec98816e733ef83812a9e88b40646e
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-12-20 14:24:18 +0100

    day 19, part 2

--- 19/advent19.cpp
+++ 19/advent19.cpp
@@ -23,6 +23,30 @@ std::optional<std::string> readInput(char const* filename)
     return sstr.str();
 }
 
+int manual_compute(int r5)
+{
+    int r0 = 0;
+    for(int r1 = 1; r1 <= r5; ++r1) {
+        for(int r3 = 1; r3 <= r5; ++r3) {
+            if(r1 * r3 == r5) {
+                r0 += r1;
+            }
+        }
+    }
+    return r0;
+}
+
+int manual_compute_fast(int r5)
+{
+    int r0 = r5;
+    for(int r1 = 1; r1 <= r5 / 2; ++r1) {
+        if(r5 % r1 == 0) {
+            r0 += r1;
+        }
+    }
+    return r0;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -39,12 +63,16 @@ int main(int argc, char* argv[])
 
     RegisterMachine const rm = parseInput(*input);
     RegisterMachine rm1 = rm;
+
     runToCompletion(rm1);
     std::cout << "First result is " << rm1.registers[0] << std::endl;
-    //RegisterMachine rm2 = rm;
-    //rm2.registers[0] = 1;
-    //runToCompletion(rm2);
-    //std::cout << "Second result is " << rm2.registers[0] << std::endl;
+
+    RegisterMachine rm2 = rm;
+    rm2.registers[0] = 1;
+    while(rm2.ip != 1) { step(rm2); }
+    int const target_value = rm2.registers[5];
+    int const result = manual_compute_fast(target_value);
+    std::cout << "Second result is " << result << std::endl;
 
     return 0;
 }
